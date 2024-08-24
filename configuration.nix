411533965d768642472167cd822a8fac749060d9
MMorokk <chelton289@gmail.com>
2024-08-24 16:40:52 +0200

    fastfetch installed

--- configuration.nix
+++ configuration.nix
@@ -154,6 +154,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    fastfetch
     luajitPackages.luarocks-nix
     git-credential-manager
     gh
