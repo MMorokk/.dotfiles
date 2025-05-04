5c6bb6e00fe889a3e08e23637412b9a2a911e309
MMorokk <51864605+MMorokk@users.noreply.github.com>
2025-05-04 13:32:34 +0200

    something

--- nvim/lua/core/defaults.lua
+++ nvim/lua/core/defaults.lua
@@ -1,17 +1,22 @@
-vim.g.mapleader = 'x'
-vim.g.maplocalleader = 'x'
+vim.g.mapleader = 'z'
+vim.g.maplocalleader = 'z'
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth= 2
+vim.opt.clipboard= "unnamed"
 
 vim.cmd [[ set noswapfile ]]
 -- vim.cmd [[ set termguicolors ]]
 
 --Line numbers
 vim.wo.number = true
+vim.wo.relativenumber = true
+
 
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
